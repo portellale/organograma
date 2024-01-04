e20519bb6cf14837647be9b9652464f9f17ca538
portellale <portella_le@hotmail.com>
2024-01-04 13:50:58 -0300

    Salvando projeto finalizado

---
+++ src/components/Banner/index.js
@@ -0,0 +1,15 @@
+import "./Banner.css";
+
+function Banner() {
+  // JSX
+  return (
+    <header className="banner">
+      <img
+        src="/images/banner.png"
+        alt="Banner principal da página do Organo"
+      />
+    </header>
+  );
+}
+
+export default Banner;
